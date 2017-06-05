01fba9f762a764ab05d54a601dae1df3f8b26c2e
butlerx <butlerx@notthe.cloud>
2017-06-05 20:50:22 +0100

    generate png rather then svg

--- docs/js/github.js
+++ docs/js/github.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   var git = 'http://github.com/redbrick/docs/edit/master/docs'
   var t1 = window.location.pathname
   var url = null
-  if (t1 == '/') {
-      url = git + '/index.md'
+  if (t1 === '/') {
+    url = git + '/index.md'
   } else {
-      url = git+t1.substr(0, t1.length-1)+'.md'
+    url = git + t1.substr(0, t1.length - 1) + '.md'
   }
-  a_git = $('[href="https://github.com/redbrick/docs"]')
-  a_git.attr('href', url).attr('target', '_blank')
+  let aGit = $('[href="https://github.com/redbrick/docs"]')
+  aGit.attr('href', url).attr('target', '_blank')
 })
--- docs/js/graphviz.js
+++ docs/js/graphviz.js
@@ -4,7 +4,7 @@ graphvizs.each(function (key, value) {
     var $value = $(value)
     var $ele = $(value).parent().parent()
 
-    var graphviz = Viz($value.text())
+    var graphviz = Viz($value.text(), { format: 'png-image-element' })
     if (!graphviz) throw Error('viz.js output empty graph')
     $value.html(graphviz)
 
